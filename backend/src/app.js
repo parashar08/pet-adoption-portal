98e88bd68dfd97761ff566ba78f34b1985731f6e
priyanshuBS <priyanshuparashar08@gmail.com>
2025-04-19 22:09:04 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 export default app;
