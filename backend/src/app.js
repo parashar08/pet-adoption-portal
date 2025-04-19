8376c2d1c1bc6a2643763767c9156bb922493b6d
priyanshuBS <priyanshuparashar08@gmail.com>
2025-04-19 22:09:57 +0530

    install and use cookieparser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -11,5 +12,6 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
 export default app;
