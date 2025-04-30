68c18bb9e1e01d47a6d09cd7a7529ed95e98d4d0
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-05-01 00:03:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://client-46yl.onrender.com',
   credentials: true,
 };
 
